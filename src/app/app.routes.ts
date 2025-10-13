32fe95f55b27b9443c8de273214335e9538b5729
Sebasdsk <80434951+Sebasdsk@users.noreply.github.com>
2025-10-13 14:51:33 -0700

    Componentes para reutilizar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Register } from './register/register';
+import { Profile } from './profile/profile';
+
 
 export const routes: Routes = [
   {path: 'login', component: Login},
-  {path: 'register', component: Register}
+  {path: 'register', component: Register},
+  {path: 'profile', component: Profile}
 ];
